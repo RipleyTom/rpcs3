f84f001b6528e430ad4cfa81a9e058302ee1c825
RipleyTom <RipleyTom@users.noreply.github.com>
2020-08-02 21:22:55 +0200

    UI update

---
+++ rpcs3/rpcs3qt/rpcn_settings_dialog.h
@@ -0,0 +1,18 @@
+#include <QDialog>
+#include <QLineEdit>
+
+class rpcn_settings_dialog : public QDialog
+{
+	Q_OBJECT
+
+public:
+	rpcn_settings_dialog(QWidget* parent = nullptr);
+
+	bool save_config();
+	bool create_account();
+
+	protected:
+	QLineEdit *edit_host, *edit_npid, *edit_pass;
+
+	std::string generate_npid();
+};
